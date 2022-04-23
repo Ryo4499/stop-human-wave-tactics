04ba3dfbe0fc4e706f501a47c6801f47acf23545
Ryo4499 <ryo.p4499@gmail.com>
2022-04-24 01:58:00 +0900

    Add: graphql
    graphql追加

---
+++ stop-human-wave-tactics-backend/config/plugins.ts
@@ -0,0 +1,12 @@
+export default {
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+};
