88b55fb26527fec6bebad15fd9fca30bb47f38f5
Ryo4499 <ryo.p4499@gmail.com>
2022-04-24 01:57:43 +0900

    Modify: change strapi typescript
    strapiをtype scriptに変更
    postgresを14に変更
    commentのテーブル削除

---
+++ stop-human-wave-tactics-backend/dist/config/database.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: env("DATABASE_HOST", "db"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "stop_human_wave_tactics_db"),
+            user: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "secret"),
+            ssl: env.bool("DATABASE_SSL", false),
+        },
+    },
+});
