c53f4d9725dcf25f8c15115649055ebb266a3982
Ryo4499 <ryo.p4499@gmail.com>
2022-05-22 12:35:50 +0900

    Modify: インデックスページの変更
    Header追加
    Footer追加
    Sidebar追加
    Articles追加
    Theme Color追加

--- stop-human-wave-tactics-frontend/pages/_app.tsx
+++ stop-human-wave-tactics-frontend/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import "../styles/globals.css"
+import Header from "../components/common/Header";
+import Fotter from "../components/common/Footer";
+import { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
