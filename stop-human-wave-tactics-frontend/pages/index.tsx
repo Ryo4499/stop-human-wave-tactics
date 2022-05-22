65bbba51bc0fd7baa967bfe8847e46c01ca4cf5a
Ryo4499 <ryo.p4499@gmail.com>
2022-05-22 12:35:50 +0900

    Modify: インデックスページの変更
    Header追加
    Footer追加
    Sidebar追加
    Articles追加
    Theme Color追加

--- stop-human-wave-tactics-frontend/pages/index.tsx
+++ stop-human-wave-tactics-frontend/pages/index.tsx
@@ -1,9 +1,47 @@
-import Header from '../components/Header'
+import Header from "../components/common/Header";
+import Sidebar from "../components/common/Sidebar";
+import Footer from "../components/common/Footer";
+import Articles from "../components/Articles/Articles";
+import Grid from "@mui/material/Grid"
+import Box from '@mui/material/Box';
+import Pagination from '@mui/material/Pagination';
+import type { InferGetStaticPropsType } from 'next';
 
-export default function App() {
-  return (
-    <div>
-      <Header></Header>
-    </div>
-  );
+export async function getStaticProps() {
+	const products = [1, 2, 3]
+
+	return {
+		props: {
+			products
+		},
+		revalidate: 4 * 60 * 60
+	}
+}
+
+export default function App({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
+	return (
+		<div>
+			<Header></Header>
+			<Grid container>
+				<Grid xs={10}>
+					<Box>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+						<Articles></Articles>
+					</Box>
+				</Grid>
+				<Grid xs={2}>
+					<Sidebar></Sidebar>
+				</Grid>
+			</Grid>
+			<Pagination count={10} color="primary" />
+			<Footer></Footer>
+		</div>
+	);
 }
