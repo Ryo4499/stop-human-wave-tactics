4cb5479ba30d600c197896ed1a517da7bef8087c
Ryo4499 <ryo.p4499@gmail.com>
2022-05-30 21:22:57 +0900

    WIP: Changing Layout
    レイアウトの変更途中

--- stop-human-wave-tactics-frontend/components/common/Footer.tsx
+++ stop-human-wave-tactics-frontend/components/common/Footer.tsx
@@ -1,18 +1,17 @@
 import Typography from '@mui/material/Typography';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 export default function ButtonAppBar() {
     return (
-        <Paper sx={{ position: "static", bottom: 0, left: 0, right: 0, mt: 3 }}>
-            <BottomNavigation>
-                <Typography
-                    variant="h6"
-                    component="div"
-                >
-                    Created By Ryo Arai At 2022
-                </Typography>
-            </BottomNavigation>
-        </Paper>
+        <Grid container justifyContent="center" mt={3}>
+            <Typography
+                variant="h6"
+                component="div"
+            >
+                Created By Ryo Arai At 2022
+            </Typography>
+        </Grid>
     );
 }
