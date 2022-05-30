f9e0b149e1dc2c50d9feba74ffcf1782299b0151
Ryo4499 <ryo.p4499@gmail.com>
2022-05-30 21:22:57 +0900

    WIP: Changing Layout
    レイアウトの変更途中

--- stop-human-wave-tactics-frontend/pages/index.tsx
+++ stop-human-wave-tactics-frontend/pages/index.tsx
@@ -2,46 +2,47 @@ import Header from "../components/common/Header";
 import Sidebar from "../components/common/Sidebar";
 import Footer from "../components/common/Footer";
 import Articles from "../components/Articles/Articles";
-import Grid from "@mui/material/Grid"
-import Box from '@mui/material/Box';
-import Pagination from '@mui/material/Pagination';
-import type { InferGetStaticPropsType } from 'next';
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Pagination from "@mui/material/Pagination";
+import type { InferGetStaticPropsType } from "next";
 
 export async function getStaticProps() {
-	const products = [1, 2, 3]
+	const products = [1, 2, 3];
 
 	return {
 		props: {
-			products
+			products,
 		},
-		revalidate: 4 * 60 * 60
-	}
+		revalidate: 4 * 60 * 60,
+	};
 }
 
-export default function App({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function App({
+	products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
 	return (
-		<div>
-			<Header></Header>
-			<Grid container>
-				<Grid xs={10}>
+		<Grid container direction="column">
+			<Grid container justifyContent="center" direction="row">
+				<Header></Header>
+			</Grid>
+			<Grid container direction="row">
+				<Grid container justifyContent="center" direction="column" xs={9} md={10} px={10}>
 					<Box>
 						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
-						<Articles></Articles>
+					</Box>
+					<Box sx={{ justifyContent: "center" }}>
+						<Pagination count={10} color="primary" />
 					</Box>
 				</Grid>
-				<Grid xs={2}>
+				<Grid container justifyContent="center" direction="column" xs={3} md={2}>
 					<Sidebar></Sidebar>
 				</Grid>
 			</Grid>
-			<Pagination count={10} color="primary" />
-			<Footer></Footer>
-		</div>
+			<Grid container justifyContent="center">
+				<Footer></Footer>
+			</Grid>
+		</Grid >
 	);
 }
