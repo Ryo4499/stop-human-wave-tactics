b3163c412515e14ae86fce2f1c965ceb468e0814
Ryo4499 <ryo.p4499@gmail.com>
2022-06-13 00:23:22 +0900

    Add: Apollo Client
    Apollo Client の追加

--- stop-human-wave-tactics-frontend/pages/_app.tsx
+++ stop-human-wave-tactics-frontend/pages/_app.tsx
@@ -3,17 +3,32 @@ import Header from "../components/common/Header";
 import Fotter from "../components/common/Footer";
 import { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+        ApolloClient,
+        InMemoryCache,
+        ApolloProvider,
+        useQuery,
+        gql
+} from "@apollo/client";
 
 const darkTheme = createTheme({
-	palette: {
-		mode: "dark",
-	},
+        palette: {
+                mode: "dark",
+        },
+});
+
+const cache = new InMemoryCache()
+const client = new ApolloClient({
+        uri: `${process.env.BACKEND_URL}/graphql`,
+        cache: cache,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider theme={darkTheme}>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+        return (
+                <ApolloProvider client={client}>
+                        <ThemeProvider theme={darkTheme}>
+                                <Component {...pageProps} />
+                        </ThemeProvider>
+                </ApolloProvider>
+        );
 }
