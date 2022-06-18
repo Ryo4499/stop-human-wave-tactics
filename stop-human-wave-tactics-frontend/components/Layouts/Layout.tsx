9dc176219ccb7170c3b5584c700a0a91be4be959
Ryo4499 <ryo.p4499@gmail.com>
2022-06-18 22:37:52 +0900

    Add: 基本のレイアウト作成
    Headerをトップ、mainをセンター、Footerをボトムに指定

---
+++ stop-human-wave-tactics-frontend/components/Layouts/Layout.tsx
@@ -0,0 +1,32 @@
+import Header from "../Common/Header"
+import Footer from "../Common/Footer"
+import Sidebar from "../Common/Sidebar"
+import React, { ReactElement } from "react"
+import { Grid, Box, Container, Item } from "@mui/material"
+
+type LayoutProps = Required<{ readonly children: ReactElement }>
+
+export default function Layout({
+        children
+}: LayoutProps) {
+        return (
+                <Grid container direction="column" alignContent="space-between">
+                        <Grid container item justifyContent="center" alignItems="flex-start">
+                                <Header />
+                        </Grid>
+                        <Grid container item direction="row" justifyContent="space-around" alignItems="stretch" sx={{ flexGrow: 1 }} p={1.5}>
+                                <Grid container item direction="row" justifyContent="space-between" spacing={5}>
+                                        <Grid item xs={10} justifyContent="center">
+                                                <main>{children}</main>
+                                        </Grid>
+                                        <Grid item xs={2} justifyContent="center">
+                                                <Sidebar />
+                                        </Grid>
+                                </Grid>
+                        </Grid>
+                        <Grid container item justifyContent="center" alignItems="flex-end">
+                                <Footer />
+                        </Grid>
+                </Grid>
+        )
+}
---
+++ stop-human-wave-tactics-frontend/pages/privacy-policy.tsx
@@ -0,0 +1,7 @@
+import { Grid, Box } from "@mui/material"
+
+export default function App() {
+        return (
+                <Box></Box>
+        )
+}
