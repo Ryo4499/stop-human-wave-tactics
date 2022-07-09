047d47a9f07dc1e378bcf7d7d7b2cb505be47cdc
Ryo4499 <ryo.p4499@gmail.com>
2022-07-10 07:00:16 +0900

    Modify: Strapi JS
    Strapi to JS

---
+++ stop-human-wave-tactics-backend/config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("POSTGRES_HOST"),
+      port: env.int("POSTGRES_PORT"),
+      database: env("POSTGRES_DB"),
+      user: env("POSTGRES_USER"),
+      password: env("POSTGRES_PASSWORD"),
+      ssl: env.bool("DB_SSL", false),
+    },
+  },
+});
