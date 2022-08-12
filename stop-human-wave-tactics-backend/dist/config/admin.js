e893abb27f7aaab2e20b2078a9725da6be50412a
Ryo4499 <ryo.p4499@gmail.com>
2022-08-12 16:57:23 +0900

    Modify: distがignoreされていなかったので追加

--- stop-human-wave-tactics-backend/dist/config/admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    url: process.env.ADMIN_URL,
-    auth: {
-        secret: process.env.ADMIN_JWT_SECRET,
-    },
-};
