dc9d1c0725617417ebd6101db54c08e7b938e911
Ryo4499 <ryo.p4499@gmail.com>
2022-08-16 03:52:15 +0900

    Add: Dynamic Routeing Article
    add dynamic routeing article details

--- stop-human-wave-tactics-frontend/components/Articles/Articles.tsx
+++ stop-human-wave-tactics-frontend/components/Articles/Articles.tsx
@@ -1,12 +1,32 @@
+import React, { SetStateAction } from "react";
+import Link from "next/link";
+import NextLink from "next/link";
+import Image from "next/image";
+import parse, {
+  attributesToProps,
+  DOMNode,
+  domToReact,
+  HTMLReactParserOptions,
+  Element,
+} from "html-react-parser";
+import { useState, ChangeEvent } from "react";
 import { useQuery } from "@apollo/client";
 import {
+  Card,
+  CardContent,
+  CardMedia,
   Grid,
   Pagination,
   PaginationItem,
+  Container,
   Link as MuiLink,
+  Typography,
+  CardActions,
+  Button,
 } from "@mui/material";
 import { getArticles } from "../../graphql/getArticles";
 import {
+  Article,
   ArticleEntity,
   ArticleFiltersInput,
   ArticleLocalizationsArgs,
@@ -17,15 +37,21 @@ import {
 } from "../../types/apollo_client";
 import { Loading } from "../Common/Loading";
 import { DisplayError } from "../Common/DisplayError";
-import { useRouter } from "next/router";
-import NextLink from "next/link";
-import { useState, ChangeEvent } from "react";
+import { NextRouter } from "next/router";
+
+interface ArticlesProps {
+  page: number;
+  setPage: (value: number) => void;
+  router: NextRouter;
+}
 
 const perPage = parseInt(process.env.PER_PAGE || "1");
 
-export const Articles = () => {
-  const router = useRouter();
-  const [page, setPage] = useState(1);
+export const Articles: React.FC<ArticlesProps> = ({
+  page,
+  setPage,
+  router,
+}) => {
   const { data, loading, error } = useQuery<
     getArticlesQuery,
     getArticlesQueryVariables
@@ -48,22 +74,70 @@ export const Articles = () => {
     setPage(value);
     router.push(`/articles/${value}`);
   };
+  console.log(data);
+  const options: HTMLReactParserOptions = {
+    replace: (domNode: DOMNode) => {
+      if (domNode instanceof Element && domNode.type === "tag") {
+        const elem = domNode;
+        const props = attributesToProps(domNode.attribs);
+        if (domNode.attribs && domNode.name === "a") {
+          return (
+            <Link href={props.href}>
+              {domToReact(domNode.children, options)}
+            </Link>
+          );
+        } else if (domNode.attribs && domNode.name == "img") {
+          return <CardMedia component="img" src={props.src}></CardMedia>;
+        }
+      }
+    },
+  };
 
   return (
-    <Grid>
-      <ul>
+    <Container>
+      <Grid>
         {data?.articles?.data.map((article) => {
           if (!article) return null;
           else {
-            return <li key={article.id}>{article.attributes?.title}</li>;
+            return (
+              <Card key={article.id}>
+                <Link href={`/article/${article.id}`}>
+                  <CardMedia
+                    component="img"
+                    image={article.attributes?.thumbnail?.data?.attributes?.url}
+                  />
+                </Link>
+                <CardContent></CardContent>
+                <Typography variant="h5" component="div">
+                  {article.attributes?.title}
+                </Typography>
+                <Typography>{article.attributes?.summary}</Typography>
+                <Typography>
+                  {article.attributes?.updatedAt
+                    .replace("T", " ")
+                    .replace(/\..*$/g, "")
+                    .replace(/\-/g, "/")}
+                </Typography>
+                <CardActions>
+                  <Link href={`/article/${article.id}`}>
+                    <Button size="small">More Details</Button>
+                  </Link>
+                </CardActions>
+              </Card>
+            );
           }
         })}
-      </ul>
+      </Grid>
       <Pagination
-        page={data?.articles?.meta.pagination.page}
+        page={page}
         count={data?.articles?.meta.pagination.pageCount}
         onChange={handleChange}
+        renderItem={(item) => (
+          <Link href={`/articles/${item.page}`} key={item.page} passHref>
+            <PaginationItem {...item} />
+          </Link>
+        )}
       ></Pagination>
-    </Grid>
+    </Container>
   );
 };
---
+++ stop-human-wave-tactics-frontend/pages/article/index.tsx
@@ -0,0 +1,49 @@
+import { NextPage } from "next";
+import { Grid } from "@mui/material";
+import { addApolloState, initializeApollo } from "../../lib/apollo";
+import {
+  getArticleQuery,
+  getArticleQueryVariables,
+} from "../../types/apollo_client";
+import { getArticle } from "../../graphql/getArticle";
+import { useRouter } from "next/router";
+import { ArticleDetails } from "../../components/Article/ArticleDetails";
+import { DisplayError } from "../../components/Common/DisplayError";
+
+const ArticlePage: NextPage = () => {
+  const router = useRouter();
+  console.log(router);
+  if (router.query.id === undefined) {
+    return (
+      <DisplayError
+        error={{
+          __typename: "Undefined ID",
+          code: "01",
+          message: "Undefined ID",
+        }}
+      ></DisplayError>
+    );
+  } else {
+    return (
+      <Grid>
+        <ArticleDetails id={router.query.id} router={router}></ArticleDetails>
+      </Grid>
+    );
+  }
+};
+
+export async function getStaticProps() {
+  const client = initializeApollo();
+  try {
+    await client.query<getArticleQuery, getArticleQueryVariables>({
+      query: getArticle,
+    });
+    return addApolloState(client, { props: {}, revalidate: 60 });
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default ArticlePage;
