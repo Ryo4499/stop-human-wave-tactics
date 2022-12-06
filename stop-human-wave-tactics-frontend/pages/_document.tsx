78ada158c0df30ef0d1c42b0d722ec2f8bcfa8a1
Ryo4499 <ryo.p4499@gmail.com>
2022-12-06 19:33:58 +0900

    Modify: custom documents

--- stop-human-wave-tactics-frontend/pages/_document.tsx
+++ stop-human-wave-tactics-frontend/pages/_document.tsx
@@ -1,22 +1,13 @@
-import Document, { DocumentContext, Head, Html, NextScript, Main } from "next/document";
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 
 class MyDocument extends Document {
-        static async getInitialProps(ctx: DocumentContext) {
-                const initialProps = await Document.getInitialProps(ctx);
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
 
-                return initialProps;
-        }
-        render() {
-                return (
-                        <Html>
-                                <Head />
-                                <body>
-                                        <Main />
-                                        <NextScript />
-                                </body>
-                        </Html>
-                );
-        }
+    return initialProps
+  }
 }
 
-export default MyDocument;
+export default MyDocument
