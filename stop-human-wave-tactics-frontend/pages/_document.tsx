3dd18e47992456626af6bc41b27c1170794a8bed
Ryo4499 <ryo.p4499@gmail.com>
2022-12-06 20:53:22 +0900

    Revert "Modify: custom documents"
    This reverts commit 178f28f09b5722584005de7fd67673e046cb9d16.

--- stop-human-wave-tactics-frontend/pages/_document.tsx
+++ stop-human-wave-tactics-frontend/pages/_document.tsx
@@ -1,13 +1,22 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, { DocumentContext, Head, Html, NextScript, Main } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
+        static async getInitialProps(ctx: DocumentContext) {
+                const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
-  }
+                return initialProps;
+        }
+        render() {
+                return (
+                        <Html>
+                                <Head />
+                                <body>
+                                        <Main />
+                                        <NextScript />
+                                </body>
+                        </Html>
+                );
+        }
 }
 
-export default MyDocument
+export default MyDocument;
