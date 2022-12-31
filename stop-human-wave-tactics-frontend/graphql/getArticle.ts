17f077a70f9684ad5b25535a249929352f40eb5d
Ryo4499 <ryo.p4499@gmail.com>
2023-01-01 03:44:40 +0900

    Modify: Apollo Query

--- stop-human-wave-tactics-frontend/graphql/getArticle.ts
+++ stop-human-wave-tactics-frontend/graphql/getArticle.ts
@@ -61,3 +61,65 @@ export const getArticle = gql`query($id: ID!, $locale: I18NLocaleCode!) {
   }
 }
 `
+
+export const getArticleLocal = gql`query($id: ID!, $locale: I18NLocaleCode!) {
+  article(id: $id, locale: $locale) @client {
+    data {
+      id
+      attributes {
+        title
+        summary
+        content
+        thumbnail {
+          data {
+            attributes {
+              name
+              alternativeText
+              caption
+              width
+              height
+              url
+              previewUrl
+            }
+          }
+        }
+        category {
+          data {
+            id
+            attributes {
+              name
+            }
+          }
+        }
+        Seo {
+          id
+          metaTitle
+          metaDescription
+          metaImage {
+            data {
+              attributes {
+                name
+                alternativeText
+                caption
+                width
+                height
+                url
+                previewUrl
+              }
+            }
+          }
+          keywords
+          metaRobots
+          structuredData
+          metaViewport
+          canonicalURL
+        }
+        createdAt
+        updatedAt
+        publishedAt
+        locale
+      }
+    }
+  }
+}
+`
--- stop-human-wave-tactics-frontend/graphql/getI18NLocales.ts
+++ stop-human-wave-tactics-frontend/graphql/getI18NLocales.ts
@@ -16,3 +16,20 @@ export const getI18NLocales = gql`query (
   }
 }
 `
+// client側のcacheを見に行く
+export const getI18NLocalesLocal = gql`query (
+  $filters: I18NLocaleFiltersInput
+  $pagination: PaginationArg
+  $sort: [String]
+) {
+  i18NLocales(filters: $filters, pagination: $pagination, sort: $sort) @client {
+    data {
+      id
+      attributes {
+        name
+        code
+      }
+    }
+  }
+}
+`
