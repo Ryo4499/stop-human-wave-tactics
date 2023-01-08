084af9ca128f1bc8dad37e093670ea4cf5fb9adf
ar44 <ryo.p4499@gmail.com>
2023-01-09 06:12:41 +0900

    Modify: split theme files

--- stop-human-wave-tactics-frontend/pages/_app.tsx
+++ stop-human-wave-tactics-frontend/pages/_app.tsx
@@ -1,21 +1,18 @@
 import "../styles/globals.css";
-import type { GetStaticProps, NextPage } from "next";
+import type { NextPage } from "next";
 import { useMediaQuery } from "@mui/material";
-import { useReducer, createContext, useContext } from "react"
+import { useReducer } from "react"
 import React from "react";
 import Layout from "../components/Layouts/Layout";
 import { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { ApolloProvider, useQuery } from "@apollo/client";
-import { addApolloState, initializeApollo, useApollo } from "../lib/apollo";
-import { GetCategoriesQuery, GetCategoriesQueryVariables } from "../types/apollo_client";
-import { getCategories } from "../graphql/getCategories";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apollo";
 import { useLocale } from "../lib/locale";
-import DisplayError from "../components/Common/DisplayError";
-import Loading from "../components/Common/Loading";
+import { darkPallete, lightPallete } from "../lib/theme";
 
-const App: NextPage = ({ Component, pageProps }: AppProps) => {
-  const [dark, toggleDark] = useReducer<boolean>((dark: boolean) => { return !dark }, true)
+const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const [dark, toggleDark] = useReducer((dark: boolean) => { return !dark }, true)
   const prefersDarkMode = useMediaQuery(`(prefers-color-scheme: ${dark ? "dark" : "light"})`, { noSsr: true });
   const { locale, locales, t } = useLocale()
   const client = useApollo(pageProps);
@@ -25,70 +22,7 @@ const App: NextPage = ({ Component, pageProps }: AppProps) => {
         palette: {
           mode: prefersDarkMode ? "dark" : "light",
           ...(prefersDarkMode === true
-            ? {
-              divider: "#382934",
-              action: {
-                active: "#484923",
-                hover: "#ddda32",
-                selected: "#83f384",
-                disabled: "#382934",
-                disabledBackground: "#339388",
-              },
-              background: {
-                default: "#888888",
-                paper: "#000000",
-              },
-              text: {
-                primary: "#DA59D9",
-                secondary: "#FF8F3F",
-              },
-              typography: {
-                fontFamily: [
-                  "-apple-system",
-                  "BlinkMacSystemFont",
-                  '"Segoe UI"',
-                  "Roboto",
-                  '"Helvetica Neue"',
-                  "Arial",
-                  "sans-serif",
-                  '"Apple Color Emoji"',
-                  '"Segoe UI Emoji"',
-                  '"Segoe UI Symbol"',
-                ].join(","),
-              },
-            }
-            : {
-              divider: "#383838",
-              action: {
-                active: "#484923",
-                hover: "#ddda32",
-                selected: "#83f384",
-                disabled: "#382934",
-                disabledBackground: "#339388",
-              },
-              background: {
-                default: "#ffffff",
-                paper: "#fffff8",
-              },
-              text: {
-                primary: "#000000",
-                secondary: "#842900",
-              },
-              typography: {
-                fontFamily: [
-                  "-apple-system",
-                  "BlinkMacSystemFont",
-                  '"Segoe UI"',
-                  "Roboto",
-                  '"Helvetica Neue"',
-                  "Arial",
-                  "sans-serif",
-                  '"Apple Color Emoji"',
-                  '"Segoe UI Emoji"',
-                  '"Segoe UI Symbol"',
-                ].join(","),
-              },
-            }),
+            ? darkPallete : lightPallete)
         },
       }),
     [prefersDarkMode]
