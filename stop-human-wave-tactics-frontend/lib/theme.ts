762d3974bb6c01cd214323cf93e5eda37a37f2a4
ar44 <ryo.p4499@gmail.com>
2023-01-09 06:12:41 +0900

    Modify: split theme files

---
+++ stop-human-wave-tactics-frontend/lib/theme.ts
@@ -0,0 +1,65 @@
+export const darkPallete = {
+    divider: "#382934",
+    action: {
+        active: "#484923",
+        hover: "#ddda32",
+        selected: "#83f384",
+        disabled: "#382934",
+        disabledBackground: "#339388",
+    },
+    background: {
+        default: "#888888",
+        paper: "#000000",
+    },
+    text: {
+        primary: "#DA59D9",
+        secondary: "#FF8F3F",
+    },
+    typography: {
+        fontFamily: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(","),
+    },
+}
+
+export const lightPallete = {
+    divider: "#383838",
+    action: {
+        active: "#484923",
+        hover: "#ddda32",
+        selected: "#83f384",
+        disabled: "#382934",
+        disabledBackground: "#339388",
+    },
+    background: {
+        default: "#ffffff",
+        paper: "#fffff8",
+    },
+    text: {
+        primary: "#000000",
+        secondary: "#842900",
+    },
+    typography: {
+        fontFamily: [
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(","),
+    },
+}
