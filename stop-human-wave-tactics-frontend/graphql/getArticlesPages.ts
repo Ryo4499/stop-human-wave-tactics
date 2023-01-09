0341543ac2fb11fb5d267c3dca1790f50a75eab4
ar44 <ryo.p4499@gmail.com>
2023-01-10 00:59:06 +0900

    Add: new graphql query
    extract ids
    extract pages

---
+++ stop-human-wave-tactics-frontend/graphql/getArticlesPages.ts
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client"
+
+export const getArticlesPages = gql`
+query($filters:ArticleFiltersInput,$pagination:PaginationArg!,$sort:[String],$publicationState:PublicationState,$locale:I18NLocaleCode!){
+  articles(filters:$filters,pagination:$pagination,sort:$sort,publicationState:$publicationState,locale:$locale){
+    meta{
+      pagination{
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`
+
+export const getArticlesPagesLocal = gql`
+query($filters:ArticleFiltersInput,$pagination:PaginationArg!,$sort:[String],$publicationState:PublicationState,$locale:I18NLocaleCode!){
+  articles(filters:$filters,pagination:$pagination,sort:$sort,publicationState:$publicationState,locale:$locale) @client {
+    meta{
+      pagination{
+        total
+        page
+        pageSize
+        pageCount
+      }
+    }
+  }
+}
+`
