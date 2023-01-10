c348d5e7dbf9372674ad92b1348f3ad84dba4e41
ar44 <ryo.p4499@gmail.com>
2023-01-10 23:47:41 +0900

    Add: Same Slug locale content

---
+++ stop-human-wave-tactics-backend/src/api/category/content-types/category/lifecycle.ts
@@ -0,0 +1,34 @@
+import slugify from "slugify"
+import { ApplicationError } from "@strapi/utils"
+
+export default {
+    async beforeCreate(event) {
+        await generateSlug(event);
+    },
+
+    async beforeUpdate(event) {
+        await generateSlug(event);
+    },
+};
+
+const generateSlug = async (event: any) => {
+    const DEFAULT_LOCALE = "en";
+    const { data } = event.params;
+    const id = event.params?.where?.id ?? null;
+    const locale = !id ? "en" : await getLocale(id);
+
+    //Generate slug for en locale only
+    if (!data.slug && data.name && locale == DEFAULT_LOCALE) {
+        data.slug = slugify(data.name, { lower: true });
+    }
+
+    if (!data.slug) {
+        throw new ApplicationError("Slug is required!");
+    }
+};
+
+const getLocale = async (id: string) => {
+    const res = await strapi.service("api::category:category").findOne(id, {});
+
+    return res.locale;
+};
