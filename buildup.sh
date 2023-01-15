8fa37a699942791dd3827475c79d164a2a48ce6d
ar44 <ryo.p4499@gmail.com>
2023-01-16 08:46:58 +0900

    Add: proxy

--- buildup.sh
+++ buildup.sh
@@ -2,8 +2,8 @@
 docker compose down
 docker compose build alertmanager prometheus exporter grafana db
 docker compose up -d alertmanager prometheus exporter grafana db
-docker compose build back
-docker compose up -d back
+docker compose build back proxy
+docker compose up -d back proxy
 docker compose build front
 docker compose up -d front
 yes | docker image prune
