a16b7434995f3c4137a37ceda8db1a3f02a9a550
ar44 <ryo.p4499@gmail.com>
2023-01-16 08:44:46 +0900

    Fix: ssl env

--- stop-human-wave-tactics-backend/config/database.ts
+++ stop-human-wave-tactics-backend/config/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
       database: env('POSTGRES_DB'),
       user: env('POSTGRES_USER'),
       password: env('POSTGRES_PASSWORD'),
-      ssl: (env('DB_SSL').toLowerCase() == "true"),
+      ssl: env.bool('DB_SSL', false),
     },
   },
 });
