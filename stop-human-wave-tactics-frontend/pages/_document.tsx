a4816477dc2393b17dfec99bcd9aeaf7c00e0405
ar44 <ryo.p4499@gmail.com>
2023-01-17 09:31:44 +0900

    Add: HackGen font

--- stop-human-wave-tactics-frontend/pages/_document.tsx
+++ stop-human-wave-tactics-frontend/pages/_document.tsx
@@ -16,16 +16,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cardo:ital@1&family=Kenia&family=Source+Code+Pro:ital@1&family=Source+Serif+Pro:ital@1&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
