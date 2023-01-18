907e3cc906acec54ade4663e57d759ef310d3659
ar44 <ryo.p4499@gmail.com>
2023-01-18 12:53:01 +0900

    Remove: custom font

--- stop-human-wave-tactics-frontend/lib/theme.ts
+++ stop-human-wave-tactics-frontend/lib/theme.ts
@@ -1,4 +1,3 @@
-import HackGen from "../fonts/HackGen35ConsoleNFJ-Regular.ttf"
 export const darkPallete = {
     divider: "#382934",
     action: {
@@ -18,7 +17,6 @@ export const darkPallete = {
     },
     typography: {
         fontFamily: [
-            "HackGen",
             "-apple-system",
             "BlinkMacSystemFont",
             '"Segoe UI"',
@@ -30,20 +28,6 @@ export const darkPallete = {
             '"Segoe UI Emoji"',
             '"Segoe UI Symbol"',
         ].join(","),
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-        @font-face {
-          font-family: 'HackGen';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('HackeGen Console NFJ-Regular'), url(${HackGen}) format('ttf');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
-        },
     },
 }
 
@@ -66,7 +50,6 @@ export const lightPallete = {
     },
     typography: {
         fontFamily: [
-            "HackGen",
             "-apple-system",
             "BlinkMacSystemFont",
             '"Segoe UI"',
@@ -78,19 +61,5 @@ export const lightPallete = {
             '"Segoe UI Emoji"',
             '"Segoe UI Symbol"',
         ].join(","),
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-        @font-face {
-          font-family: 'HackGen';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('HackeGen Console NFJ-Regular'), url(${HackGen}) format('ttf');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
-        },
     },
 }
