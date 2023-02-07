7d271c6a03cb2dfee1028cc9f5cce5b046be40ca
ar44 <ryo.p4499@gmail.com>
2023-02-07 10:54:51 +0900

    Fix: next image size

--- stop-human-wave-tactics-frontend/components/Common/CustomImage.tsx
+++ stop-human-wave-tactics-frontend/components/Common/CustomImage.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 import Grid from "@mui/material/Unstable_Grid2"
-import path from "path"
-import { getBackendURL, getProxyURL } from '../../lib/graphqlClient'
 
 interface CustomImage {
     src: string
@@ -11,6 +9,6 @@ interface CustomImage {
     height: number
 }
 const CustomImage: React.FC<CustomImage> = props => {
-    return <Grid container sx={{ position: "relative" }}><Image src={props.src} width={props.width ? props.width : 1} height={props.height ? props.height : 1} alt={props.alt} style={{ width: "auto", height: "auto", maxWidth: "100%", maxHeight: "100%", objectFit: "contain" }} /></Grid>
+    return <Grid container sx={{ position: "relative" }}><Image src={props.src} className="nextimage" fill alt={props.alt} /></Grid>
 }
 export default CustomImage
