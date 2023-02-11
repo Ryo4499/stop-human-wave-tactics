bd7625bd0431e0464a843efd7e6ec35f66955e97
ar44 <ryo.p4499@gmail.com>
2023-02-12 02:50:06 +0900

    Fix: image size

--- stop-human-wave-tactics-frontend/components/Common/CustomLink.tsx
+++ stop-human-wave-tactics-frontend/components/Common/CustomLink.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import { Typography } from '@mui/material'
 const CustomLink: React.FC<{ href: string, children: any, id: string, className: string }> = ({ href, children, id, className }) => {
     const isMyPageLink = href.startsWith("/") || href.startsWith("#") || href === ""
     return isMyPageLink ? (
-        <Link href={href} id={id} className={className} >
+        <Link href={href} id={id} className={className} color="text.link" >
             {children}
         </Link>
     ) : (
         <a href={href} target="_blank" rel="noopener noreferrer" id={id} className={className}>
-            {children}
-        </a>
+            <Typography color="text.link">
+                {children}
+            </Typography>
+        </a >
     )
 }
 export default CustomLink
