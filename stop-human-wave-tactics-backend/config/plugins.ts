2cf9251d0231ae299288c5c8edd2dba94a7dd40c
ar44 <ryo.p4499@gmail.com>
2023-02-16 20:18:19 +0900

    Update: node version

--- stop-human-wave-tactics-backend/config/plugins.ts
+++ stop-human-wave-tactics-backend/config/plugins.ts
@@ -18,22 +18,22 @@ export default ({ env }) => ({
       sendMetadata: true,
     }
   },
-  //upload: {
-  //  config: {
-  //    provider: 'aws-s3',
-  //    providerOptions: {
-  //      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-  //      secretAccessKey: env('AWS_ACCESS_SECRET'),
-  //      region: env('AWS_REGION'),
-  //      params: {
-  //        Bucket: env('AWS_BUCKET'),
-  //      },
-  //    },
-  //    actionOptions: {
-  //      upload: {},
-  //      uploadStream: {},
-  //      delete: {},
-  //    },
-  //  },
-  //},
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
