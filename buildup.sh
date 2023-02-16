b2e96e55b977050dbec8e2edfe8c602e6fb8acf9
ar44 <ryo.p4499@gmail.com>
2023-02-17 04:35:00 +0900

    Modify: docker compose command

--- buildup.sh
+++ buildup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker compose down
-docker compose build --no-cache alertmanager prometheus exporter grafana db certbot
-docker compose up -d alertmanager prometheus exporter grafana db certbot
-docker compose build --no-cache back proxy
-docker compose up -d back proxy
-docker compose build --no-cache front
-docker compose up -d front
+docker-compose down
+docker-compose build --no-cache alertmanager prometheus exporter grafana db certbot
+docker-compose up -d alertmanager prometheus exporter grafana db certbot
+docker-compose build --no-cache back proxy
+docker-compose up -d back proxy
+docker-compose build --no-cache front
+docker-compose up -d front
 yes | docker image prune
