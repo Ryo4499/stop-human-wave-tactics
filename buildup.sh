1ad66f3577685405fe1f028a6bf840ddde031cbb
ar44 <ryo.p4499@gmail.com>
2023-02-17 04:40:43 +0900

    Fix: container up order

--- buildup.sh
+++ buildup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker-compose down
-docker-compose build --no-cache alertmanager prometheus exporter grafana db certbot
+docker-compose build --no-cache alertmanager prometheus exporter grafana db
 docker-compose up -d alertmanager prometheus exporter grafana db certbot
-docker-compose build --no-cache back proxy
+docker-compose build --no-cache back proxy certbot
 docker-compose up -d back proxy
 docker-compose build --no-cache front
 docker-compose up -d front
