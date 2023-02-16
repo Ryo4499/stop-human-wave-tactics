cf43e7b2b458860180a490a79ac063b82578c5b1
ar44 <ryo.p4499@gmail.com>
2023-02-17 04:09:06 +0900

    Add: AWS S3

--- stop-human-wave-tactics-backend/src/plugin.js
+++ stop-human-wave-tactics-backend/src/plugin.js
@@ -3,8 +3,4 @@ module.exports = ({ env }) => ({
         enabled: true,
         resolve: "./src/plugins/strapi-plugin-ckeditor"
     },
-    uuid: {
-        enabled: true,
-        resolve: "./src/plugins/uuid"
-    }
 });
--- stop-human-wave-tactics-backend/src/plugins/strapi-plugin-field-uuid/admin/src/components/Uuid/UuidIcon/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import icon from './icon.svg'
-
-const UuidIcon = () => <img alt={'uuid'} src={icon} />
-
-export default UuidIcon
