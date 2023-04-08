d37b24949feba440870245cb36d50463f4b8f798
ar44 <ryo.p4499@gmail.com>
2023-04-08 20:51:42 +0900

    Modify: prod conf

--- buildup.sh
+++ buildup.sh
@@ -2,8 +2,8 @@
 docker compose down
 docker compose build alertmanager prometheus exporter grafana db
 docker compose up -d alertmanager prometheus exporter grafana db
-docker compose build back proxy certbot
-docker compose up -d back proxy certbot
+docker compose build back certbot proxy
+docker compose up -d back certbot proxy
 docker compose build front
 docker compose up -d front
 yes | docker image prune
--- certbot/update-cert.sh
+++ certbot/update-cert.sh
@@ -5,4 +5,4 @@ CURRENT=$(
 )
 echo $CURRENT
 source $CURRENT/../.env
-docker run --mount type=bind,source=$CURRENT/letsencrypto,target=/etc/letsencrypto --mount type=bind,source=$CURRENT/html,target=/var/www/html --rm certbot/certbot:latest certonly --webroot -w /var/www/html -d $DOMAIN --agree-tos --email ar44.blog@gmail.com
+docker run --mount type=bind,source=$CURRENT/letsencrypto,target=/etc/letsencrypto --mount type=bind,source=$CURRENT/html,target=/var/www/html --rm certbot/certbot:latest certonly --webroot -w /var/www/html -d $DOMAIN --agree-tos --email $EMAIL
