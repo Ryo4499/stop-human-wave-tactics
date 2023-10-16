00178a5ee24fe1820d3151a433678018fa6bb774
x-o-o44dev <ryo.p4499@gmail.com>
2023-10-16 22:03:10 +0900

    Fix: backend

--- stop-human-wave-tactics-backend/config/plugins.ts
+++ stop-human-wave-tactics-backend/config/plugins.ts
@@ -1,81 +1,59 @@
-import fs from "fs"
+import fs from "fs";
 export default ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
-      endpoint: '/graphql',
+      endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: true,
+      playgroundAlways: env("MODE") === "DEV" ? true : false,
       depthLimit: 25,
       amountLimit: 30,
-    }
+    },
   },
   ckeditor: true,
   seo: { enabled: true },
   sentry: {
     enabled: true,
     config: {
-      dsn: fs.readFileSync(env('SENTRY_DSN'),"utf8").replace("\n",""),
+      dsn: fs.readFileSync(env("SENTRY_DSN"), "utf8").replace("\n", ""),
       sendMetadata: true,
-    }
+    },
   },
-  content-versioning: {
-		enabled:  true,
-	},
-  chartbrew: true,
-  localazy: {
+  "content-versioning": {
+    enabled: true,
+  },
+  migrations: {
+    enabled: true,
     config: {
-      /**
-       * both options may help guard against DoS attacks
-       * if `populateMaxDepth` < 5; the Localazy Strapi Plugin may not work as expected
-       */
-      populateDefaultDepth:5, // default is 5
-      populateMaxDepth: 10, // default is 10
+      autoStart: true,
+      migrationFolderPath: "migrations",
     },
   },
   translate: {
     enabled: true,
     config: {
-      provider: 'deepl',
+      provider: "deepl",
       providerOptions: {
         // your API key - required and wil cause errors if not provided
-        apiKey: fs.readFileSync(env("DEEPL_API_KEY"),"utf8").replace("\n",""),
+        apiKey: fs.readFileSync(env("DEEPL_API_KEY"), "utf8").replace("\n", ""),
         // use custom api url - optional
-        apiUrl: 'https://api-free.deepl.com',
+        apiUrl: "https://api-free.deepl.com",
         apiOptions: {
           // see <https://github.com/DeepLcom/deepl-node#text-translation-options> for supported options.
           // note that tagHandling Mode cannot be set this way.
           // use with caution, as non-default values may break translation of markdown
-          formality: 'default',
-        }
+          formality: "default",
+        },
       },
     },
     translatedFieldTypes: [
-      'string',
-      { type: 'text', format: 'plain' },
-      { type: 'richtext', format: 'markdown' },
-      'component',
-      'dynamiczone',
+      "string",
+      { type: "text", format: "plain" },
+      { type: "richtext", format: "markdown" },
+      "component",
+      "dynamiczone",
     ],
     // If relations should be translated (default true)
     translateRelations: true,
   },
-  //upload: {
-  //  config: {
-  //    provider: 'aws-s3',
-  //    providerOptions: {
-  //      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-  //      secretAccessKey: env('AWS_ACCESS_SECRET'),
-  //      region: env('AWS_REGION'),
-  //      params: {
-  //        Bucket: env('AWS_BUCKET'),
-  //      },
-  //    },
-  //    actionOptions: {
-  //      upload: {},
-  //      uploadStream: {},
-  //      delete: {},
-  //    },
-  //  },
-  //},
 });
