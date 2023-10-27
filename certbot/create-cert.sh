5f132e75d730abe615f56d53f4e82406fb21364b
Ryo4499 <ryo.p4499@gmail.com>
2023-10-27 18:59:00 +0900

    Modify: prod conf

---
+++ certbot/create-cert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CURRENT=$(
+    cd $(dirname $0)
+    pwd
+)
+echo $CURRENT
+source $CURRENT/../.env
+
+docker compose exec certbot certbot certonly --webroot -w /usr/share/nginx/html --email $EMAIL -d $DOMAIN --agree-tos
