da74901f312aba14859e79c28367db8ca3b1491b
Ryo4499 <ryo.p4499@gmail.com>
2023-11-07 00:32:16 +0900

    Fix: adsense, analytics

--- stop-human-wave-tactics-frontend/lib/gtag.ts
+++ stop-human-wave-tactics-frontend/lib/gtag.ts
@@ -1,28 +1,8 @@
 export const GA_TRACKING_ID = process.env.GTAG
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-  // @ts-ignore
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
-  });
-};
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({
-  action,
-  category,
-  label,
-  value,
-}: {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
-}) => {
-  // @ts-ignore
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
   });
 };
