fb7339ae1601afab80e778336c6df5db304fa905
Ryo4499 <ryo.p4499@gmail.com>
2023-11-07 19:15:19 +0900

    Add: backup.sh

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+BACKUP_DIR='./backup'
+today=$(date "+%Y%m%d")
+mkdir -p $BACKUP_DIR
+docker compose exec -it db pg_dump -U $(cat .secrets/DB_USER) $(cat .secrets/DB_NAME) | gzip > "${BACKUP_DIR}/${today}_db_bk.gz"
