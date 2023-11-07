070addef99b8e6fa1f8911371c856c87f0b20556
Ryo4499 <ryo.p4499@gmail.com>
2023-11-07 20:42:15 +0900

    Modify: cd script dir

--- restore.sh
+++ restore.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+CURRENT=$(
+    cd $(dirname $0)
+    pwd
+)
+echo $CURRENT
 # Need to gunzip backup file 
 if [ -e $1 ]; then
 	docker exec -it db psql -U $(cat .secrets/DB_USER) -d $(cat .secrets/DB_NAME) < $1;
