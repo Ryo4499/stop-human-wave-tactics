52c27abc6ff9a4b8080c7b7232c6f0a5cb18f247
Ryo4499 <ryo.p4499@gmail.com>
2023-11-09 00:39:16 +0900

    Fix: adsense and analytics

--- stop-human-wave-tactics-frontend/pages/_document.tsx
+++ stop-human-wave-tactics-frontend/pages/_document.tsx
@@ -4,6 +4,8 @@ import Document, {
   NextScript,
   Main,
 } from "next/document";
+import Script from "next/script"
+import { GA_ID } from "../lib/gad";
 import GoogleAnalytics from "../components/Common/Analytics";
 
 class MyDocument extends Document {
@@ -11,8 +13,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="google-adsense-account" content={GA_ID} />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark-dimmed.min.css" integrity="sha512-zcatBMvxa7rT7dDklfjauWsfiSFParF+hRfCdf4Zr40/MmA1gkFcBRbop0zMpvYF3FmznYFgcL8wlcuO/GwHoA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+          <Script id="google_adsense" data-ad-client={GA_ID} async strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
           <GoogleAnalytics />
         </Head>
         <body>
