d5b1960e79833a51de6a78bda6710f1f92d4bb38
x-o-o44dev <ryo.p4499@gmail.com>
2024-03-19 23:24:07 +0900

    Fix: img path

--- stop-human-wave-tactics-frontend/components/utils/Head.tsx
+++ stop-human-wave-tactics-frontend/components/utils/Head.tsx
@@ -26,9 +26,7 @@ const Meta = ({ title, description, keyword }: Props): React.ReactNode => {
       <meta http-equiv="Cache-Control" content="public, max-age=0" />
       <meta name="msapplication-config" content="/browserconfig.xml" />
       <link rel="canonical" href={getProxyURL()} />
-      <link rel="icon" type="image/x-icon" href="/staic/images/favicon.ico" sizes="any" />
-      <link rel="icon" type="image/jpg" sizes="32x32" href="/staic/images/favicon_32x32.jpg" />
-      <link rel="icon" type="image/jpg" sizes="16x16" href="/staic/images/favicon_16x16.jpg" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="any" />
       <link rel="apple-touch-icon" href={getProxyURL()} />
     </Head>
   );
