08e579ab5ba2b67ccd5c636f7ed87dd7862d84d9
x-o-o44dev <ryo.p4499@gmail.com>
2024-03-22 14:18:45 +0900

    Modify: observe app

--- buildup.sh
+++ buildup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 docker compose down
-docker compose build alertmanager prometheus exporter grafana db
-#docker compose up -d alertmanager prometheus exporter grafana 
+docker compose build alertmanager prometheus exporter grafana cadvisor nginx-exporter db
+docker compose up -d alertmanager prometheus exporter grafana cadvisor
 docker compose up -d db
 docker compose build back certbot proxy 
 docker compose up -d back certbot proxy
 docker compose build front 
 docker compose up -d front
-yes | docker system prune
+docker compose up -d nginx-exporter
+yes | docker image prune
