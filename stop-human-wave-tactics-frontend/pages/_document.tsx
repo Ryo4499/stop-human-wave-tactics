0da70ddc95c782fc1b1688ca7b97cad5f751bc28
Ryo4499 <ryo.p4499@gmail.com>
2024-03-23 03:45:56 +0900

    Fix: google adsense

--- stop-human-wave-tactics-frontend/pages/_document.tsx
+++ stop-human-wave-tactics-frontend/pages/_document.tsx
@@ -4,7 +4,6 @@ import Document, {
   NextScript,
   Main,
 } from "next/document";
-import Script from 'next/script'
 import { getGaId } from "../lib/google";
 
 class MyDocument extends Document {
@@ -15,9 +14,13 @@ class MyDocument extends Document {
           <meta name="google-adsense-account" content={getGaId()} />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark-dimmed.min.css" integrity="sha512-zcatBMvxa7rT7dDklfjauWsfiSFParF+hRfCdf4Zr40/MmA1gkFcBRbop0zMpvYF3FmznYFgcL8wlcuO/GwHoA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-          <Script id="google_adsense" src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${getGaId()}`} crossOrigin="anonymous" />
+          <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${getGaId()}`} crossOrigin="anonymous" />
+          <script custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js" />
         </Head>
         <body>
+          <amp-auto-ads type="adsense"
+            data-ad-client={getGaId()}>
+          </amp-auto-ads>
           <Main />
           <NextScript />
         </body>
