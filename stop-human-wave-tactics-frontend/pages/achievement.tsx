dd19d7cca600c134da7858788c2ce1aca17a3939
Ryo4499 <ryo.p4499@gmail.com>
2024-03-26 00:20:24 +0900

    Remove: useEffect

--- stop-human-wave-tactics-frontend/pages/achievement.tsx
+++ stop-human-wave-tactics-frontend/pages/achievement.tsx
@@ -1,11 +1,9 @@
 import { request } from "graphql-request";
 import Grid from "@mui/material/Unstable_Grid2";
 import Link from "next/link";
-import { useEffect } from "react"
 import Typography from "@mui/material/Typography";
 import type { NextPage } from "next";
 import useSWR from "swr";
-import { useRouter } from "next/router";
 import { getBackendGraphqlURL } from "../lib/graphqlClient";
 import { getCategories } from "../graphql/getCategories";
 import { CategoryEntity, CategoryEntityResponseCollection } from "../types/graphql_res";
@@ -163,19 +161,6 @@ const Achievement: NextPage<CategoriesResponseProps> = ({
     },
     fallbackData: { categories: categories, variables: variables },
   });
-  const router = useRouter()
-  useEffect(() => {
-    router.beforePopState(({ as }) => {
-      if (as !== router.asPath) {
-        return false
-      }
-      return true;
-    });
-
-    return () => {
-      router.beforePopState(() => true);
-    };
-  }, [router]);
   if (data != null) {
     return (
       <Grid container sx={{ flexGrow: 1 }}>
