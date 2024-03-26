204e94daa48367304cf40b89b6c1771d9808579e
x-o-o44dev <ryo.p4499@gmail.com>
2024-03-26 23:16:02 +0900

    Modify: MODE > NODE_ENV

--- buildup.sh
+++ buildup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker compose down
 docker compose build alertmanager prometheus exporter grafana cadvisor nginx-exporter k6 influxdb db
-docker compose up -d alertmanager prometheus exporter grafana cadvisor k6 influxdb
+docker compose up -d alertmanager prometheus exporter grafana cadvisor 
 docker compose up -d db
 docker compose build back certbot proxy 
 docker compose up -d back certbot proxy
