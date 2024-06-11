cd97d53a1dfba34589926227589eb21a53c9829d
Ryo4499 <ryo.p4499@gmail.com>
2024-06-12 02:22:56 +0900

    Add: github actions workflows

---
+++ run-workflows.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+gh variable set -f .env
+gh variable delete REPO_URL
+gh variable delete ACCESS_TOKEN
+gh variable delete RUNNER_WORKDIR
+gh variable delete LABELS
+
+# ar44 @ MAINDESK in ~/promgramming/ne
+array=( $(find .secrets -type f) )
+for i in ${array[@]}
+do
+    name=$(basename $i)
+    value=$(cat $i)
+    gh secret set $name -b $value
+done
+
+gh workflow run .github/workflows/linter.yml --ref feat/github_actions
