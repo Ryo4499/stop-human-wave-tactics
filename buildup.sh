2b6818874fc33da4edb777576064c1f470a0be40
Ryo4499 <ryo.p4499@gmail.com>
2024-07-07 19:22:08 +0900

    update: favicon

--- buildup.sh
+++ buildup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker compose down
 yes | docker system prune --volumes 
-docker compose build alertmanager prometheus exporter grafana nginx-exporter back certbot proxy
-docker compose up -d alertmanager prometheus exporter grafana db back certbot proxy
-docker compose build front 
+docker compose build alertmanager prometheus exporter grafana nginx-exporter back certbot proxy --no-cache
+docker compose up -d alertmanager prometheus exporter grafana db back certbot proxy 
+docker compose build front --no-cache
 docker compose up -d front nginx-exporter
